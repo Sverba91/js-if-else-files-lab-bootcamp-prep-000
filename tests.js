7d452e1b456de0a634f6fae949a109bc6bd7b154
Sofya Verba <sofyaverba@gmail.com>
2017-10-19 01:09:57 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,10 +3,10 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "Joe")
-  }
+    console.log("Expected: Susan, Received: Joe")
+}
 if (index.height === "70") {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "74")
+    console.log("Expected: 70, Received: 74")
 }
